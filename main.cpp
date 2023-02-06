2317aeeb0af0d83557487adbc3e4706ff94943e1
hindawi <ahmedhindawi2001@gmail.com>
2023-02-06 18:47:09 +0200

    very basic first version

--- main.cpp
+++ main.cpp
@@ -1,14 +1,47 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 queue<string> tasksQ;
+
 map<string, string> cmds = {
-    {"ls", "List tasks"},
-    {"add", "Add a task"},
-    {"rm", "Remove a task"},
-    {"help", "Show this help message"},
-    {"exit", "Exit the program"}
+    {"ls", "List tasks."},
+    {"add", "Add a task."},
+    {"done", "Current task done."},
+    {"exit", "Exit the program."}
 };
+
+void listTasks() {
+    for (auto& cmd : cmds) {
+        cout << cmd.first << " => " << cmd.second << endl;
+    }
+}
+
+typedef void (*cmdFunc)();
+
+
+void printCurrTask() {
+    cout << tasksQ.front() << endl;
+}
+
+
+
+void addTask() {
+    string task;
+    cout << "task: ";
+    cin >> task;
+    tasksQ.push(task);
+}
+
+void exit() {
+    ofstream tasks("tasks.txt");
+    while(!tasksQ.empty()) {
+        tasks << tasksQ.front() << endl;
+        tasksQ.pop();
+    }
+    tasks.close();
+}
+
 
 void checkTasksFile()
 {
@@ -19,7 +52,33 @@ void checkTasksFile()
     {
         ofstream newfile(filename);
     }
+
 }
+
+void currTask() {
+    if(tasksQ.empty()) {
+        cout << "Not tasks yet, Try adding new task by typing \"add\"" << endl;
+    } else {
+        cout << tasksQ.front() << endl;
+    }
+}
+
+void taskDone() {
+    string currTask = tasksQ.front();
+    tasksQ.pop();
+    cout << currTask << " done!" << endl;
+}
+
+map<string,cmdFunc> InitializeTasksFunctions() {
+    map<string,cmdFunc> tasksFunctions;
+    tasksFunctions["ls"] = &listTasks;
+    tasksFunctions["add"] = &addTask;
+    tasksFunctions["curr"] = &currTask;
+    tasksFunctions["exit"] = &exit;
+    tasksFunctions["done"] = &taskDone;
+    return tasksFunctions;
+}
+
 
 void load()
 {
@@ -32,6 +91,17 @@ void load()
     }
 }
 
+
 int main()
 {
+    load();
+    map<string,cmdFunc> tasksFunctions = InitializeTasksFunctions();
+    string cmd;
+    while(cmd.empty()) {
+        cin >> cmd;
+        tasksFunctions[cmd]();
+        if(cmd == "exit") break;
+        cmd.clear();
+    }
+    return 0;
 }
